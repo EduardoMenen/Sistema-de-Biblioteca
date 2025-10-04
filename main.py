535fd2f557e4d76507ff9b6e62982dd4be7eaa7a
EduardoMenen <Eduardoriboli12@hotmail.com>
2025-10-04 16:00:40 -0300

    Primeiro Commit:  Estrutura do projeto

---
+++ main.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+class Item_biblioteca(ABC):
+    pass
+class Livro(Item_biblioteca):
+    pass
+class Revista(Item_biblioteca):
+    pass
+class DVD(Item_biblioteca):
+    pass
